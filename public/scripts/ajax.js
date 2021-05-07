edde8c5aa4b789298949f84f07b2ac1b6820e4f0
abhsurek <abhsurek@cisco.com>
2021-05-08 00:15:02 +0530

    ajax over view engine

--- public/scripts/ajax.js
+++ public/scripts/ajax.js
@@ -1,46 +1,63 @@
 $(function(){
     var district_data_block = "";
     $("input#subscribe").click((event) => {
-        event.preventDefault();
-        var data = $("form#preferences").serialize()+"&subscribe=Subscribe";
-        $.ajax({
-            url: "action",
-            type: "post",
-            data: data,
-            timeout: 20000,
-        }).done((result) => {
-            console.log(result);
-            if (result.length) {
-                var alert_data = "<div class=\"alert alert-success alert-dismissible fade show\" role=\"alert\">" +
-                    "                <strong>" + result + "</strong>" +
-                    "                <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button>" +
-                    "            </div>";
-                $("#alert_placeholder").append(alert_data);
-            }
-        }).fail(function (jqXHR, textStatus, error) { alert("Unable to fetch data at this time | " + textStatus + " | " + error); });;
+        $("#age").attr("required", true);
+        $("#email").attr("required", true);
+        $("#location_value").attr("required", true);
+        if ($("#age").val().length != 0 && $("#email").val().length != 0 && $("#location_value").val().length != 0) {
+            event.preventDefault();
+            var data = $("form#preferences").serialize() + "&subscribe=Subscribe";
+            $.ajax({
+                url: "action",
+                type: "post",
+                data: data,
+                timeout: 20000,
+            }).done((result) => {
+                console.log(result);
+                if (result.length) {
+                    var alert_data = "<div class=\"alert alert-success alert-dismissible fade show\" role=\"alert\">" +
+                        "                <strong>" + result + "</strong>" +
+                        "                <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button>" +
+                        "            </div>";
+                    $("#alert_placeholder").append(alert_data);
+                }
+            }).fail(function (jqXHR, textStatus, error) {
+                alert("Unable to fetch data at this time | " + textStatus + " | " + error);
+            });
+        }
     });
     $("input#unsubscribe").click((event) => {
-        event.preventDefault();
-        var data = $("form#preferences").serialize()+"&unsubscribe=Unsubscribe";
-        $.ajax({
-            url: "action",
-            type: "post",
-            data: data,
-            timeout: 20000,
-        }).done((result) => {
-            console.log(result);
-            if (result.length) {
-                var alert_data = "<div class=\"alert alert-warning alert-dismissible fade show\" role=\"alert\">" +
-                    "                <strong>" + result + "</strong>" +
-                    "                <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button>" +
-                    "            </div>";
-                $("#alert_placeholder").append(alert_data);
-            }
-        }).fail(function (jqXHR, textStatus, error) { alert("Unable to fetch data at this time | " + textStatus + " | " + error); });
+        $("#age").removeAttr("required");
+        $("#email").attr("required", true);
+        $("#location_value").removeAttr("required");
+        if ($("#email").val().length != 0) {
+            event.preventDefault();
+            var data = $("form#preferences").serialize() + "&unsubscribe=Unsubscribe";
+            $.ajax({
+                url: "action",
+                type: "post",
+                data: data,
+                timeout: 20000,
+            }).done((result) => {
+                console.log(result);
+                if (result.length) {
+                    var alert_data = "<div class=\"alert alert-warning alert-dismissible fade show\" role=\"alert\">" +
+                        "                <strong>" + result + "</strong>" +
+                        "                <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button>" +
+                        "            </div>";
+                    $("#alert_placeholder").append(alert_data);
+                }
+            }).fail(function (jqXHR, textStatus, error) {
+                alert("Unable to fetch data at this time | " + textStatus + " | " + error);
+            });
+        }
     });
     $("input#check_availability").click((event) => {
-        event.preventDefault();
+        $("#age").attr("required", true);
+        $("#email").removeAttr("required");
+        $("#location_value").attr("required", true);
         if ($("#age").val().length != 0 && $("#location_value").val().length != 0) {
+            event.preventDefault();
             var data = $("form#preferences").serialize()+"&check_availability=Check Availability";
             $.ajax({
                 url: "action",
@@ -105,19 +122,4 @@ $(function(){
             $("#location_value").append(district_data_block);
         }
     }
-    $("#unsubscribe").click(function() {
-        $("#age").removeAttr("required");
-        $("#email").attr("required", true);
-        $("#location_value").removeAttr("required");
-    });
-    $("#subscribe").click(function() {
-        $("#age").attr("required", true);
-        $("#email").attr("required", true);
-        $("#location_value").attr("required", true);
-    });
-    $("#check_availability").click(function() {
-        $("#age").attr("required", true);
-        $("#email").removeAttr("required");
-        $("#location_value").attr("required", true);
-    });
 });
