dbbdf02deabf252acd9f9e292ba9c8b1bd117b46
abhsurek <abhsurek@cisco.com>
2021-05-09 21:49:00 +0530

    fixing email

--- email_content.js
+++ email_content.js
@@ -7,7 +7,7 @@ let emailContentFormatter = async(centerFilteredData) => {
         for (j = 0; j < centerFilteredData[i].sessions.length; j++) {
             let subrow = `
                 <tr>
-                    <th scope="row">` + (j+1) + `</th>
+                    <td>` + (j+1) + `</td>
                     <td>` + centerFilteredData[i].sessions[j].date + `</td>
                     <td>` + centerFilteredData[i].sessions[j].min_age_limit + `</td>
                     <td>` + centerFilteredData[i].sessions[j].available_capacity + `</td>
@@ -18,15 +18,15 @@ let emailContentFormatter = async(centerFilteredData) => {
         }
         let row = `
             <tr>
-                <th>` + (i + 1) + `</th>
+                <td>` + (i + 1) + `</td>
                 <td>` + centerFilteredData[i].name + `</td>
                 <td>` + centerFilteredData[i].address + `</td>
                 <td>` + centerFilteredData[i].district_name + `</td>
                 <td>` + centerFilteredData[i].pincode + `</td>
                 <td>
-                    <table class="table">
-                        <thead class="thead-dark">
-                            <tr>
+                    <table>
+                        <thead>
+                            <tr style="width:100%">
                                 <th>#</th>
                                 <th>Date</th>
                                 <th>Min Age</th>
@@ -64,7 +64,7 @@ let emailContentFormatter = async(centerFilteredData) => {
                     padding-top: 12px;
                     padding-bottom: 12px;
                     text-align: left;
-                    background-color: #03161d;
+                    background-color: #343a40;
                     color: white;
                 }
                 .additionalStyles{
@@ -77,12 +77,12 @@ let emailContentFormatter = async(centerFilteredData) => {
                 <table id="records" align="center">
                     <thead class="thead-dark">
                         <tr>
-                            <th scope="col" style="width:3%">#</th>
-                            <th scope="col" style="width:14%">Center Name</th>
-                            <th scope="col" style="width:18%">Address</th>
-                            <th scope="col" style="width:8%">District</th>
-                            <th scope="col" style="width:10%">Pincode</th>
-                            <th scope="col" style="width:67%">Sessions</th>
+                            <th style="width:3%">#</th>
+                            <th style="width:14%">Center Name</th>
+                            <th style="width:18%">Address</th>
+                            <th style="width:8%">District</th>
+                            <th style="width:10%">Pincode</th>
+                            <th style="width:67%">Sessions</th>
                         </tr>
                     </thead>
                     <tbody>` + rows + `</tbody>
